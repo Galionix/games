5fc27f7981a13d054ecf0421d02f9303a9d61c55
galionix <galionix2@gmail.com>
2023-10-08 19:37:38 +0300

    init

---
+++ workspaces/client/src/index.ts
@@ -0,0 +1,37 @@
+import { DisplayMode, Engine, Loader } from 'excalibur';
+
+import { Player } from './actors/player/player';
+import { Resources } from './resources';
+import { LevelOne } from './scenes/level-one/level-one';
+
+/**
+ * Managed game class
+ */
+class Game extends Engine {
+  private player: Player;
+  private levelOne: LevelOne;
+
+  constructor() {
+    super({ displayMode: DisplayMode.FitScreen });
+  }
+
+  public start() {
+
+    // Create new scene with a player
+    this.levelOne = new LevelOne();
+    this.player = new Player();
+    this.levelOne.add(this.player);
+
+    game.add('levelOne', this.levelOne);
+
+    // Automatically load all default resources
+    const loader = new Loader(Object.values(Resources));
+
+    return super.start(loader);
+  }
+}
+
+const game = new Game();
+game.start().then(() => {
+  game.goToScene('levelOne');
+});
---
+++ workspaces/client/src/resources.ts
@@ -0,0 +1,13 @@
+import { ImageSource } from 'excalibur';
+
+import sword from '../assets/sword.png';
+
+/**
+ * Default global resource dictionary. This gets loaded immediately
+ * and holds available assets for the game.
+ */
+const Resources = {
+    Sword: new ImageSource(sword)
+}
+
+export { Resources }
